e6714bfff1ec781d3189faa9156cffa2f049fac1
chrisssxuu <chris01.xu@gmail.com>
2018-08-08 18:02:10 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -24,16 +24,15 @@ end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
-  student_names.first
+  STUDENT_NAMES.first
 end
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
-  student_names.last
+  STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  student_names.first
-  
+  STUDENT_NAMES[0-2]
 end
